9691cfd5c4a3f57621dcbfd98e0a93516170b8d6
Xiloid <78012011+Xiloid@users.noreply.github.com>
2021-01-31 17:51:41 +0200

    Update numbers_.py

--- numbers_.py
+++ numbers_.py
@@ -5,20 +5,15 @@
     2. Вывести на экран, порядок, в котором расположены цифры числа
         (возростание/убывание/в разброс(равны))
 """
-
-print('Введите число от 1 до 999')
-a = input('Число: ')
 try:
-    a = int(a)
+    num = int(input('Число от 1 до 999: '))
 except ValueError:
     exit('Введено НЕ число!')
-
 result = units = dozens = hundreds = 0
-
-if 0 < a < 1000:
-    units = a % 10
-    if 9 < a < 100:
-        dozens = a // 10
+if 0 < num < 1000:
+    units = num % 10
+    if 9 < num < 100:
+        dozens = num // 10
         if units > dozens:
             print('Числа по возрастанию')
         elif units < dozens:
@@ -27,15 +22,17 @@ if 0 < a < 1000:
             print('Числа равны')
         result = units + dozens
         print('Сумма всех чисел:', result)
-    elif 99 < a < 1000:
-        dozens = a % 100 // 10
-        hundreds = a // 100
+    elif 99 < num < 1000:
+        dozens = num % 100 // 10
+        hundreds = num // 100
         if units < dozens < hundreds or units == dozens < hundreds or units < dozens == hundreds:
             print('Числа по убыванию')
         elif hundreds < dozens < units or units == dozens > hundreds or units > dozens == hundreds:
             print('Числа по возрастанию')
         elif units == dozens and dozens == hundreds:
             print('Числа равны')
+        else:
+            print('Числа в разнобой')
         result = units + dozens + hundreds
         print('Сумма всех чисел:', result)
     else:
